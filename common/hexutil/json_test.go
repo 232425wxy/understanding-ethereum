8f7614d94afec84a5f37a3c9f552c06f51704578
232425wxy <1378406814@mail.nwpu.edu.cn>
2022-10-26 23:52:28 +0800

    json.go还没有完成分析

--- common/hexutil/json_test.go
+++ common/hexutil/json_test.go
@@ -1,1 +1,16 @@
 package hexutil
+
+import (
+	"github.com/stretchr/testify/assert"
+	"testing"
+)
+
+func TestBytes_MarshalText(t *testing.T) {
+	b := Bytes([]byte{'a', 'b', 'c'})
+	res, err := b.MarshalText()
+	assert.Nil(t, err)
+	t.Log(res)
+	// 'a' -> 97 -> 64+32+1 -> 01100001
+	// 54 -> 32+16+4+2 ->      00110110
+	// 49 -> 32+16+1 ->        00110001
+}
