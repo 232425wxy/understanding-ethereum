61405039f9e10a2e5d46ab3b447ce2cb84aa5189
232425wxy <1378406814@mail.nwpu.edu.cn>
2022-11-06 00:57:19 +0800

    RawValue

--- rlp/raw.go
+++ rlp/raw.go
@@ -4,4 +4,7 @@ package rlp
 
 // RawValue ♏ |作者：吴翔宇| 🍁 |日期：2022/11/6|
 //
-// RawValue
+// RawValue 官方对其解释是：
+//
+//	RawValue代表一个已编码的RLP值，可用于延迟RLP解码或预先计算一个编码。请注意，解码器并不验证RawValues的内容是否是有效的RLP。
+type RawValue []byte
