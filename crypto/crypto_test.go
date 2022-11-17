aec18cae4c066946c69eba2b71d17db4ee4474e6
232425wxy <1378406814@mail.nwpu.edu.cn>
2022-11-17 16:09:40 +0800

    更新了README

--- crypto/crypto_test.go
+++ crypto/crypto_test.go
@@ -7,8 +7,15 @@ import (
 )
 
 func TestBlake2b(t *testing.T) {
-	h := crypto.BLAKE2b_256.New()
-	size := h.Size()
-	t.Log(size)
+	h256 := crypto.BLAKE2b_256.New()
+	bz256 := h256.Sum([]byte("ethereum"))
+	t.Log(h256.Size(), len(bz256), bz256)
 
+	h384 := crypto.BLAKE2b_384.New()
+	bz384 := h384.Sum([]byte("ethereum"))
+	t.Log(h384.Size(), len(bz384), bz384)
+
+	h512 := crypto.BLAKE2b_512.New()
+	bz512 := h512.Sum([]byte("ethereum"))
+	t.Log(h512.Size(), len(bz512), bz512)
 }
