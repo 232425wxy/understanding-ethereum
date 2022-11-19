b4aa91da7aaf4dd5d46e965538ac035e835846fa
232425wxy <1378406814@mail.nwpu.edu.cn>
2022-11-19 14:58:38 +0800

    bls12381

---
+++ crypto/bls12381/utils.go
@@ -0,0 +1,27 @@
+package bls12381
+
+import (
+	"errors"
+	"math/big"
+)
+
+func bigFromHex(hex string) *big.Int {
+	return new(big.Int).SetBytes(common.FromHex(hex))
+}
+
+// decodeFieldElement expects 64 byte input with zero top 16 bytes,
+// returns lower 48 bytes.
+func decodeFieldElement(in []byte) ([]byte, error) {
+	if len(in) != 64 {
+		return nil, errors.New("invalid field element length")
+	}
+	// check top bytes
+	for i := 0; i < 16; i++ {
+		if in[i] != byte(0x00) {
+			return nil, errors.New("invalid field element top bytes")
+		}
+	}
+	out := make([]byte, 48)
+	copy(out[:], in[16:])
+	return out, nil
+}
